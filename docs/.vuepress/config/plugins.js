b7b70a616997a7f9378349e9752d47beb549a257
书读的少_想的太多 <1764714737@qq.com>
2021-12-01 17:18:53 +0800

    :tada:初始化项目

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,110 +1,165 @@
 // 插件配置
 module.exports = [
-  // 本地插件
-  // [require('./plugins/love-me'), { // 鼠标点击爱心特效
-  //   color: '#11a8cd', // 爱心颜色，默认随机色
-  //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
-  // }],
+    // 本地插件
+    // [require('./plugins/love-me'), { // 鼠标点击爱心特效
+    //   color: '#11a8cd', // 爱心颜色，默认随机色
+    //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+    // }],
 
-  'vuepress-plugin-baidu-autopush', // 百度自动推送
+    'vuepress-plugin-baidu-autopush', // 百度自动推送
 
-  // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-  [
-    'thirdparty-search',
-    {
-      thirdparty: [
-        // 可选，默认 []
+    // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+    [
+        'thirdparty-search',
         {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+            thirdparty: [
+                // 可选，默认 []
+                {
+                    title: '在MDN中搜索',
+                    frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+                    behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+                },
+                {
+                    title: '在Runoob中搜索',
+                    frontUrl: 'https://www.runoob.com/?s=',
+                },
+                {
+                    title: '在Vue API中搜索',
+                    frontUrl: 'https://cn.vuejs.org/v2/api/#',
+                },
+                {
+                    title: '在Bing中搜索',
+                    frontUrl: 'https://cn.bing.com/search?q=',
+                },
+                {
+                    title: '通过百度搜索本站的',
+                    frontUrl: 'https://www.baidu.com/s?wd=site%3Alangyamu.cn%20',
+                },
+            ],
         },
-        {
-          title: '在Runoob中搜索',
-          frontUrl: 'https://www.runoob.com/?s=',
-        },
-        {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        },
-        {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q=',
-        },
-        {
-          title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-        },
-      ],
-    },
-  ],
+    ],
 
-  [
-    'one-click-copy',
-    {
-      // 代码块复制按钮
-      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-      duration: 1000, // prompt message display time.
-      showInMobile: false, // whether to display on the mobile side, default: false.
-    },
-  ],
-  [
-    'demo-block',
-    {
-      // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-      settings: {
-        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-        // cssLib: ['http://xxx'], // 在线示例中的css依赖
-        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-        jsfiddle: false, // 是否显示 jsfiddle 链接
-        codepen: true, // 是否显示 codepen 链接
-        horizontal: false, // 是否展示为横向样式
-      },
-    },
-  ],
-  [
-    'vuepress-plugin-zooming', // 放大图片
-    {
-      selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
-      options: {
-        bgColor: 'rgba(0,0,0,0.6)',
-      },
-    },
-  ],
-  [
-    'vuepress-plugin-baidu-tongji', // 百度统计
-    {
-      hm: '503f098e7e5b3a5b5d8c5fc2938af002',
-    },
-  ],
-  [
-    'vuepress-plugin-comment', // 评论
-    {
-      choosen: 'gitalk',
-      options: {
-        clientID: 'a6e1355287947096b88b',
-        clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'xugaoyi', // GitHub仓库所有者
-        admin: ['xugaoyi'], // 对仓库有写权限的人
-        // distractionFreeMode: true,
-        pagerDirection: 'last', // 'first'正序 | 'last'倒序
-        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-        body:
-          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-      },
-    },
-  ],
-  [
-    '@vuepress/last-updated', // "上次更新"时间格式
-    {
-      transformer: (timestamp, lang) => {
-        const dayjs = require('dayjs') // https://day.js.org/
-        return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
-      },
-    },
-  ],
+    // 代码块复制按钮
+    [
+        'one-click-copy',
+        {
+            // 代码块复制按钮
+            copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+            copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+            duration: 1000, // prompt message display time.
+            showInMobile: false, // whether to display on the mobile side, default: false.
+        },
+    ],
+    // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+    [
+        'demo-block',
+        {
+            settings: {
+                // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+                // cssLib: ['http://xxx'], // 在线示例中的css依赖
+                // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+                jsfiddle: false, // 是否显示 jsfiddle 链接
+                codepen: true, // 是否显示 codepen 链接
+                horizontal: false, // 是否展示为横向样式
+            },
+        },
+    ],
+    // 放大图片
+    [
+        'vuepress-plugin-zooming',
+        {
+            selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+            options: {
+                bgColor: 'rgba(0,0,0,0.6)',
+            },
+        },
+    ],
+    // 百度统计
+    // [
+    //     'vuepress-plugin-baidu-tongji',
+    //     {
+    //         hm: '503f098e7e5b3a5b5d8c5fc2938af002',
+    //     },
+    // ],
+    // 评论
+    // [
+    //     'vuepress-plugin-comment', // 评论
+    //     {
+    //         choosen: 'gitalk',
+    //         options: {
+    //             clientID: 'a6e1355287947096b88b',
+    //             clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
+    //             repo: 'blog-gitalk-comment', // GitHub 仓库
+    //             owner: 'xugaoyi', // GitHub仓库所有者
+    //             admin: ['xugaoyi'], // 对仓库有写权限的人
+    //             // distractionFreeMode: true,
+    //             pagerDirection: 'last', // 'first'正序 | 'last'倒序
+    //             id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+    //             title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+    //             labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+    //             body:
+    //                 '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+    //         },
+    //     },
+    // ],
+    // "上次更新"时间格式
+    [
+        '@vuepress/last-updated', // "上次更新"时间格式
+        {
+            transformer: (timestamp, lang) => {
+                const dayjs = require('dayjs') // https://day.js.org/
+                return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+            },
+        },
+    ],
+    // 看板娘
+    [
+        'vuepress-plugin-helper-live2d', {
+        // 是否开启控制台日志打印(default: false)
+        log: false,
+        live2d: {
+            // 是否启用(关闭请设置为false)(default: true)
+            enable: true,
+            // 模型名称(default: hibiki)>>>取值请参考：
+            // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+            model: 'hijiki',
+            display: {
+                position: "right", // 显示位置：left/right(default: 'right')
+                width: 135, // 模型的长度(default: 135)
+                height: 300, // 模型的高度(default: 300)
+                hOffset: 130, //  水平偏移(default: 65)
+                vOffset: -60, //  垂直偏移(default: 0)
+            },
+            mobile: {
+                show: false // 是否在移动设备上显示(default: false)
+            },
+            react: {
+                opacity: 0.8 // 模型透明度(default: 0.8)
+            }
+        }
+    }
+    ],
+    // 鼠标点击特效
+    [
+        "vuepress-plugin-cursor-effects",
+        {
+            size: 2,                    // size of the particle, default: 2
+            shape: 'circle',  // shape of the particle, default: 'star'
+            zIndex: 999999999           // z-index property of the canvas, default: 999999999
+        }
+    ],
+    // 樱花插件
+    [
+        "sakura",
+        // https://github.com/JabinPeng/vuepress-plugin-sakura
+        {
+            num: 20,  // 默认数量
+            show: true, //  是否显示
+            zIndex: -1,   // 层级
+            img: {
+                replace: false,  // false 默认图 true 换图 需要填写httpUrl地址
+                httpUrl: '...'     // 绝对路径
+            }
+        }
+    ]
 ]
