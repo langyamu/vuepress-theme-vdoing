05931afed67d74f54a784c371bdd8a1dbda00292
书读的少_想的太多 <1764714737@qq.com>
2021-12-02 15:11:26 +0800

    :lipstick:

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
+echo 'langyamu.cn' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:langyamu/vuepress-theme-vdoing.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://langyamu:${GITHUB_TOKEN}@github.com/langyamu/vuepress-theme-vdoing.git
+  git config --global user.name "langyamu"
+  git config --global user.email "1764714737@qq.com"
 fi
 git init
 git add -A
